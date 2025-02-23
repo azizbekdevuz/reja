a102e20586ad114f82af04432721bb9ab4b32a6e
Azizbek Arzikulov <azizbek.dev.ac@gmail.com>
2025-02-23 21:18:56 +0900

    feat: Callback & Async Masalasini yechish

--- train.js
+++ train.js
@@ -1,34 +1,65 @@
-console.log("Jack Ma maslahatlari");
+console.log("MIT25 | LEO [Azizbek]");
+console.log("Train server run success!");
 
-const list = [
-    "be a good student", //0-20
-    "choose a right teacher and make more mistakes", //20-30
-    "start working for yourself", //30-40
-    "do what you are good at", //40-50
-    "invest in the youth", //50-60
-    "rest now!", //60~
-];
+//A-Task
+/* 2 parametrli function tuzing: 1- parametrdagi letterlarni 2 parametrdagi so'zda qatnashgan sonini return qilsin.
+Masalan, countLetter("e", "engineer") 3 ni return qiladi.
+*/
 
-function advice(a, callback) {
-    if(typeof a !== 'number') callback("Please insert number", null);
-    else if(a <= 20) callback(null, list[0]);
-    else if(a > 20 && a <= 30) callback(null, list[1]);
-    else if(a > 30 && a <= 40) callback(null, list[2]);
-    else if(a > 40 && a <= 50) callback(null, list[3]);
-    else if(a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setInterval(function () {
-            callback(null, list[5]);
-        }, 1000);
-    }
+//Masalani yechimi
+function countLetter(letter, word) {
+    let count = 0;
+    for(let i = 0; i < word.length; i++){
+        if(word[i] === letter) {
+            count++;
+        };
+    };
+    console.log(count);
+    return count;
 };
 
-console.log('passed here 0');
-advice(67, (err, data) => {
-    if(err) console.log('ERROR:', err);
-    console.log('javob:', data);
-});
-console.log('passed here 1');
+countLetter("a", "internatsionalizatsiyalashtirolmayotganliklaringizdandirmikina");
+
+
+
+
+
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+
+// const list = [
+//     "be a good student", //0-20
+//     "choose a right teacher and make more mistakes", //20-30
+//     "start working for yourself", //30-40
+//     "do what you are good at", //40-50
+//     "invest in the youth", //50-60
+//     "rest now!", //60~
+// ];
+
+// // CALLBACK function
+// function advice(a, callback) {
+//     if(typeof a !== 'number') callback("Please insert number", null);
+//     else if(a <= 20) callback(null, list[0]);
+//     else if(a > 20 && a <= 30) callback(null, list[1]);
+//     else if(a > 30 && a <= 40) callback(null, list[2]);
+//     else if(a > 40 && a <= 50) callback(null, list[3]);
+//     else if(a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         }, 1000);
+//     }
+// };
+
+// console.log('passed here 0');
+// advice(67, (err, data) => {
+//     if(err) console.log('ERROR:', err);
+//     console.log('javob:', data);
+// });
+// console.log('passed here 1');
 
 
 
@@ -46,6 +77,7 @@ console.log('passed here 1');
 
 
 
+// ASYNC fuction
 // async function advice(a) {
 //     if(typeof a !== 'number') throw new Error("Please insert number");
 //     else if(a <= 20) return list[0];
@@ -61,7 +93,7 @@ console.log('passed here 1');
 //     })
 // }};
 
-//then/catch
+// call via then/catch
 // console.log('passed here 0');
 // advice(67)
 //     .then((data) => {
@@ -72,7 +104,7 @@ console.log('passed here 1');
 //     });
 // console.log("passed here 1");
 
-//async/await
+// call via async/await
 // async function run() {
 //     let javob = await advice(24);
 //     console.log(javob);
