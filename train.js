7d138a7f79229000749174fbda8616a68b649ece
Azizbek Arzikulov <azizbek.dev.ac@gmail.com>
2025-03-03 16:36:53 +0900

    C-Task Solved!

--- train.js
+++ train.js
@@ -1,37 +1,105 @@
 console.log("MIT25 | LEO [Azizbek]");
 console.log("Train server run success!");
 
+// C-Task
+/*
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+*/
+
+//Masalani yechimi
+class Shop {
+    constructor(non, lagmon, cola) {
+      //Dastlabki mahsulotlar
+      this.non = non;
+      this.lagmon = lagmon;
+      this.cola = cola;
+    }
+  
+    //Hozirgi vaqt - HH:MM uchun metod
+    getTime() {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, '0'); // Soatni olamiz> agar 1 xonali bo‘lsa> oldiga 0 qo‘shamiz (ushbu functionlar JSda built in bor ekan!!)
+      const minutes = String(now.getMinutes()).padStart(2, '0'); // Daqiqalarni olamiz> agar 1 xonali bo‘lsa> oldiga 0 qo‘shamiz (bu ham JS da built in bor!!)
+      return `${hours}:${minutes}`;
+    }
+  
+    //Omborda qancha mahsulot borligini aytuvchi metod
+    qoldiq() {
+      const time = this.getTime(); //Hozirgi vaqt
+      return `Hozir ${time}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`;
+    }
+  
+    //Mahsulot sotish uchun function
+    sotish(mahsulot, soni) {
+      //Agar mahsulot mavjud bo'lsa va yetarli soni bo‘lsa sotamiz
+      if (this[mahsulot] !== undefined) {
+        if (this[mahsulot] >= soni) {
+          this[mahsulot] -= soni; //Ombordan remove qilamiz, chunki sotildi
+          console.log(`Hozir ${this.getTime()}da ${soni}ta ${mahsulot} sotildi.`);
+        } else {
+          console.log(`Hozir ${this.getTime()}da yetarli ${mahsulot} yo‘q!`); //Mahsulot yetmasa
+        }
+      } else {
+        console.log(`Hozir ${this.getTime()}da bunday mahsulot mavjud emas!`); //Nomi noto'g'ri kiritilsa
+      }
+    }
+  
+    //Omborga yangi mahsulot olish uchun function
+    qabul(mahsulot, soni) {
+      //Agar mahsulot ro‘yxatda bo‘lsa, unga sonini qo'shamiz
+      if (this[mahsulot] !== undefined) {
+        this[mahsulot] += soni; //Omborga add qilamiz bu safar
+        console.log(`Hozir ${this.getTime()}da ${soni}ta ${mahsulot} qabul qilindi.`);
+      } else {
+        console.log(`Hozir ${this.getTime()}da bunday mahsulot mavjud emas!`); //Nomi noto'g'ri kiritilsa
+      }
+    }
+  }
+  
+  //Masala yechimini ishlatish
+  const shop = new Shop(4, 5, 2); //4ta non, 5ta lag'mon, 2ta cola
+  console.log(shop.qoldiq()); //Omborda qancha mahsulot borligini ko'ramiz
+  
+  shop.sotish('non', 3); //3ta non sotish
+  shop.qabul('cola', 4); //4ta cola qabul
+  
+  console.log(shop.qoldiq()); //Yana qoldiqni tekshiramiz  
+
+
+
+
 // B-Task
 /* Shunday function tuzing, u 1 ta string parametrga ega bo'lsin, hamda o'sha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN, countDigits("ad2a54y79wet0sfgb9") 7 ni return qiladi.
 */
 
 //Masalani yechimi
-function countDigits(word) {
-    let count = 0;
-    let digits = [];
-    if (typeof word !== "string") {
-        console.log("Only strings are supported in this 'Masala'");
-        return null;
-    }
-    else if (typeof word === "string") {
-        for (let i = 0; i < word.length; i++) {
-            if (word[i] >= '0' && word[i] <= '9') {
-                count++;
-                digits.push(word[i]);
-            };
-        };
-        console.log(`Number of digits in '${word}' - ${count}`);
-        console.log("~ ~ ~ | ~ ~ ~");
-        console.log("Digits extracted from the list below:");
-        console.log(digits);
-        console.log("~ ~ ~ | ~ ~ ~");
-        console.log("MASALA (Task-B) solved success!!!!!");
-        return count;
-    };    
-};
+// function countDigits(word) {
+//     let count = 0;
+//     let digits = [];
+//     if (typeof word !== "string") {
+//         console.log("Only strings are supported in this 'Masala'");
+//         return null;
+//     }
+//     else if (typeof word === "string") {
+//         for (let i = 0; i < word.length; i++) {
+//             if (word[i] >= '0' && word[i] <= '9') {
+//                 count++;
+//                 digits.push(word[i]);
+//             };
+//         };
+//         console.log(`Number of digits in '${word}' - ${count}`);
+//         console.log("~ ~ ~ | ~ ~ ~");
+//         console.log("Digits extracted from the list below:");
+//         console.log(digits);
+//         console.log("~ ~ ~ | ~ ~ ~");
+//         console.log("MASALA (Task-B) solved success!!!!!");
+//         return count;
+//     };    
+// };
 
-countDigits("ad2a54y79wet0sfgb9");
+// countDigits("ad2a54y79wet0sfgb9");
 
 
 
